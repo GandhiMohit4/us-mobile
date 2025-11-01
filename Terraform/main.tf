a4255453162d91ac92445ce31ee90f4a9f29bd95
Mohit Gandhi <mohitgandhi401@gmail.com>
2025-11-01 18:03:25 -0400

    Iteration

---
+++ Terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+    required_providers {
+        aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.31"
+        }
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+    shared_credentials_file = "~/.aws/credentials"
+}
+
+
+# Create an ECR repository to store our application's Docker image
+resource "aws_ecr_repository" "app-ecr-repo" {
+    name                 = "${var.app_name}-repo"
+    image_tag_mutability = "MUTABLE"
+
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+
+}
